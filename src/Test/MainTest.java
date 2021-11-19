72c92f2238939605a24a61d10dbdea1ec66fcad6
gta98 <me@glub.xyz>
2021-11-19 11:27:44 +0200

    Added Eclipse

---
+++ src/Test/MainTest.java
@@ -0,0 +1,12 @@
+package Test;
+
+import java.io.IOException;
+
+public class MainTest {
+	
+	AVLTree t;
+	
+	public static void main(String[] args) throws IOException{
+		System.out.println("aaaa");
+	}
+}
